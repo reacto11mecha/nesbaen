f825981d54d6a3c577a9664d2b895f595b41ae6f
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-25 20:56:00 +0700

    Mengubah handler pesan

--- handler/message.js
+++ handler/message.js
@@ -1,6 +1,6 @@
 let absensi = [];
 
-const GITHUB_URL = "https://github.com/reacto11mecha/nesbaen-wa-bot";
+const GITHUB_URL = "https://github.com/reacto11mecha/nesbaen";
 
 const messageHandler = (client) => async (message) => {
   const { body, from, id, sender } = message;
@@ -18,90 +18,7 @@ const messageHandler = (client) => async (message) => {
     .split(/ +/)
     .slice(1);
 
-  async function absen() {
-    if (args.length < 1)
-      return client.reply(from, "Masukkan id absen terlebih dahulu!", id);
-
-    const absensiID = absensi.findIndex((x) => x.id === args[0]);
-
-    if (absensiID < 0)
-      return client.reply(from, `Tidak ada absensi dengan id "${args[0]}"`, id);
-
-    const sudahAbsen =
-      absensi[absensiID].lists.findIndex(
-        (list) => list.username === username
-      ) !== -1;
-
-    if (sudahAbsen)
-      return client.reply(from, "Kamu sudah absen sebelumnya, tenang.", id);
-
-    absensi = absensi.map((absen) => {
-      if (absen.id === args[0]) {
-        return {
-          ...absen,
-          lists: [...absen.lists, { username, phoneNumber: from }],
-        };
-      }
-
-      return absen;
-    });
-
-    await client.reply(from, "Berhasil diabsenkan!", id);
-    console.log(`[Absensi] Berhasil diabsenkan dengan username : ${username}`);
-  }
-
-  async function buatAbsen() {
-    if (args.length < 1)
-      return client.reply(from, "Masukkan id absen terlebih dahulu!", id);
-
-    const absensiID = absensi.findIndex((x) => x.id === args[0]);
-
-    if (absensiID > 0)
-      return client.reply(
-        from,
-        `Absensi dengan id "${args[0]}" sudah ada!`,
-        id
-      );
-
-    absensi = [...absensi, { id: args[0], lists: [] }];
-
-    await client.reply(from, "ID absensi baru berhasil ditambahkan!", id);
-    console.log(
-      `[Absensi] ID absensi baru berhasil ditambahkan! ID : ${args[0]}`
-    );
-  }
-
-  async function reporting() {
-    if (args.length < 1)
-      return client.reply(from, "Masukkan id absen terlebih dahulu!", id);
-
-    const absensiID = absensi.findIndex((x) => x.id === args[0]);
-
-    if (absensiID < 0)
-      return client.reply(from, `Tidak ada absensi dengan id "${args[0]}"`, id);
-
-    const replyMessage = `Absen id : ${args[0]}\n${
-      absensi[absensiID].lists.length > 0
-        ? `Nama-nama yang absen\n\n${absensi[absensiID].lists.map(
-            (data, idx) => `${idx + 1}.${data.username} ${data.phoneNumber}`
-          )}`
-        : "Belum ada yang absen."
-    }`;
-    await client.reply(from, replyMessage, id);
-  }
-
   switch (command) {
-    case "absen":
-      return await absen();
-
-    case "buat":
-    case "buat-absen":
-      return await buatAbsen();
-
-    case "report":
-    case "reporting":
-      return await reporting();
-
     default:
       await client.reply(
         from,
