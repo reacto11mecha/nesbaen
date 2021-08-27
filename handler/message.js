5ed2a4049f6877df96d369c87946ffcc691df054
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-27 20:00:24 +0700

    Menambahkan userNumber untuk mendapatkan no telepon wa dari pengirim

--- handler/message.js
+++ handler/message.js
@@ -5,8 +5,8 @@ const GITHUB_URL = "https://github.com/reacto11mecha/nesbaen";
 const handler = emitHandler();
 
 const messageHandler = (client) => async (message) => {
-  const { body, from, id, sender } = message;
-  const { pushname: username } = sender;
+  const { body, from, id } = message;
+  const userNumber = from.replace("@c.us", "");
 
   const command = body
     .slice(process.env.PREFIX.length)
@@ -21,18 +21,20 @@ const messageHandler = (client) => async (message) => {
     .slice(1);
 
   switch (command) {
+    case "a":
     case "absen":
-      return await handler.emit("absen", { args, client });
+      return await handler.emit("absen", { args, client, userNumber });
     case "buat":
     case "generate":
-      return await handler.emit("generate", { client });
+      return await handler.emit("generate", { client, userNumber });
     default:
       await client.reply(
         from,
         command.length > 0
           ? `Tidak ada perintah yang bernama '${command}'`
           : `Nesbaen, saya adalah bot absen.\n\nGithub: ${GITHUB_URL}`,
-        id
+        id,
+        true
       );
   }
 
