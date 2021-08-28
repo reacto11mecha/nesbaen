5baf9532fed95e34140be467e909db2f21507d73
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-28 20:20:43 +0700

    Membuat absen_id dengan default value uuid ver 4

--- models/absen.js
+++ models/absen.js
@@ -1,25 +1,27 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+
+const userListSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  precense_time: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
 const Absen = new mongoose.Schema({
   absen_id: {
     type: String,
+    default: uuidv4,
   },
   asignnator: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
-  userList: [
-    {
-      user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-      precense_time: {
-        type: Date,
-        default: new Date(),
-      },
-    },
-  ],
+  userList: [userListSchema],
   created_at: {
     type: Date,
     default: new Date(),
