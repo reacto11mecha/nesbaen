5343aae47384fd6fd0b33295f5e2bc42e455bad1
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-09-04 09:18:52 +0700

    refactor, memindahkan handler ke file controller message

--- routes/message.js
+++ routes/message.js
@@ -1,214 +1,26 @@
 import EventEmitter from "events";
 
-import Absen from "../models/absen.js";
-import Class from "../models/class.js";
-import { isStudent, isManager } from "../validator/authorization.js";
-import { isUUID4 } from "../validator/argumentsValidator.js";
-import { generateDateString } from "../utils/date.js";
-import { checkUser } from "../utils/permittedOrNot.js";
-
 import { helpReply } from "../common/message.js";
+import controller from "../controller/message.js";
 
 export default function initEmitter() {
   const messageHandler = new EventEmitter();
 
-  messageHandler.on("absen", async ({ args, client, message, userNumber }) => {
-    const permitted = await isStudent({ client, message, userNumber });
+  messageHandler.on("absen", controller.absen);
 
-    if (permitted.isPermitted) {
-      const valid = await isUUID4({ args, client, message });
+  messageHandler.on("generate", controller.generate);
 
-      if (valid) {
-        const absensi = await Absen.findOne({ absen_id: args[0] });
+  messageHandler.on("lists", controller.lists);
 
-        if (!absensi)
-          return await client.reply(
-            message.from,
-            "Absensi tidak ada !",
-            message.id,
-            true
-          );
+  messageHandler.on("delete", controller.delete);
 
-        const alreadyPresence = absensi.userList.some(({ user_id }) =>
-          user_id.equals(permitted.user._id)
-        );
-
-        if (alreadyPresence)
-          return await client.reply(
-            message.from,
-            "Anda sudah absen sebelumnya.",
-            message.id,
-            true
-          );
-
-        await client.simulateTyping(message.from, true);
-        absensi.userList.push({ user_id: permitted.user._id });
-
-        await absensi.save();
-        await client.simulateTyping(message.from, false);
-
-        return await client.reply(
-          message.from,
-          "Berhasil terabsen 👍",
-          message.id,
-          true
-        );
-      }
-    }
-  });
-
-  messageHandler.on("generate", async ({ client, message, userNumber }) => {
-    const permitted = await isManager({ client, message, userNumber });
-
-    if (permitted.isPermitted) {
-      await client.simulateTyping(message.from, true);
-      const absensi = new Absen({ assignator: permitted.user._id });
-
-      await absensi.save();
-      await client.simulateTyping(message.from, false);
-
-      await client.reply(
-        message.from,
-        "Salin perintah absen di bawah ini, kirimkan ke teman anda.",
-        message.id,
-        true
-      );
-
-      return await client.sendText(
-        message.from,
-        `${process.env.PREFIX} absen ${absensi.absen_id}`
-      );
-    }
-  });
-
-  messageHandler.on("lists", async ({ args, client, message, userNumber }) => {
-    const permitted = await isManager({ client, message, userNumber });
-
-    if (permitted.isPermitted) {
-      const valid = await isUUID4({ args, client, message });
-
-      if (valid) {
-        await client.simulateTyping(message.from, true);
-        const absensi = await Absen.findOne({ absen_id: args[0] })
-          .populate("userList.user_id")
-          .populate("assignator")
-          .lean();
-
-        if (!absensi) {
-          await client.simulateTyping(message.from, false);
-          return await client.reply(
-            message.from,
-            "Absensi tidak ada !",
-            message.id,
-            true
-          );
-        }
-
-        await client.simulateTyping(message.from, false);
-        return await client.reply(
-          message.from,
-          `LIST ABSENSI\n\nAbsen dibuat oleh: ${
-            absensi.assignator.name
-          }\nTanggal dibuat: ${generateDateString(absensi.created_at)}
-
-${
-  absensi.userList.length > 0
-    ? `Daftar Yang Hadir:${absensi.userList.map(
-        (user, idx) => `\n${idx + 1}. ${user.user_id.name}`
-      )}`
-    : "Belum ada yang absen."
-}`.trim(),
-          message.id,
-          true
-        );
-      }
-    }
-  });
-
-  messageHandler.on("delete", async ({ args, client, message, userNumber }) => {
-    const permitted = await isManager({ client, message, userNumber });
-
-    if (permitted.isPermitted) {
-      const valid = await isUUID4({ args, client, message });
-
-      if (valid) {
-        await client.simulateTyping(message.from, true);
-        const absensi = await Absen.findOne({ absen_id: args[0] })
-          .populate("assignator")
-          .lean();
-
-        if (!absensi) {
-          await client.simulateTyping(message.from, false);
-          return await client.reply(
-            message.from,
-            "Absensi tidak ada !",
-            message.id,
-            true
-          );
-        }
-
-        if (!absensi.assignator._id.equals(permitted.user._id)) {
-          await client.simulateTyping(message.from, false);
-          return await client.reply(
-            message.from,
-            "Anda bukan pembuat absen aslinya !",
-            message.id,
-            true
-          );
-        }
-
-        await Absen.deleteOne({
-          absen_id: absensi.absen_id,
-          assignator: absensi.assignator._id,
-        });
-
-        await client.simulateTyping(message.from, false);
-        return await client.reply(
-          message.from,
-          `Berhasil menghapus absen 👍\nID: ${absensi.absen_id}`,
-          message.id,
-          true
-        );
-      }
-    }
-  });
+  messageHandler.on("me", controller.me);
 
   messageHandler.on(
     "help",
     async ({ client, message }) =>
       await client.reply(message.from, helpReply, message.id, true)
   );
-
-  messageHandler.on("me", async ({ client, message, userNumber }) => {
-    const user = await checkUser({ client, message, userNumber });
-
-    if (user) {
-      const gradeClassName = user.className
-        ? await Class.findOne({ _id: user.className.grade }).lean()
-        : null;
-
-      const className = gradeClassName
-        ? gradeClassName.classNames.find((classes) =>
-            classes._id.equals(user.className.gradeName)
-          )
-        : null;
-
-      return await client.reply(
-        message.from,
-        `Profil anda
-
-Nama: ${user.name}
-${
-  gradeClassName
-    ? `Kelas: ${gradeClassName.gradeName} ${className.name}\n`
-    : "\n"
-}Roles: ${user.roles.join(", ")}
-Tanggal didaftarkan: ${generateDateString(user.created_at)}`,
-        message.id,
-        true
-      );
-    }
-  });
 
   return messageHandler;
 }
