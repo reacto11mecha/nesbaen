c92f3b548271dcd1a188706ff89dd045b35cab8b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-02-10 06:48:40 +0700

    feat: menambahkan husky,eslint, dan prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+   "root": true, 
+   "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": ["eslint:recommended", "prettier"],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
